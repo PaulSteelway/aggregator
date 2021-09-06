f2cc66797880f620afe3de349f2f89ba7a98ec4d
PaulSteelway <lewap8@gmail.com>
2021-09-06 20:01:40 +0300

    init

---
+++ application/config/email.php
@@ -0,0 +1,21 @@
+<?php
+
+// $config['protocol'] = 'mail';
+// $config['charset'] = 'utf8';
+// $config['mailtype'] = 'html';
+// $config['wordwrap'] = TRUE;
+
+// Uncomment the code below and delete the previous one to use the SMTP
+
+// /* SMTP mail
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = 'mail.good-price.com.ua';
+$config['smtp_port'] = '465';
+$config['smtp_user'] = 'admin@good-price.com.ua';
+$config['smtp_pass'] = 'Platform112358';
+$config['charset'] = 'utf8';
+$config['mailtype'] = 'html';
+$config['wordwrap'] = TRUE;
+// */
+
+?>
